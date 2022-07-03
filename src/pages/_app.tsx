c81c12647817c777266ec20c5b13d4d3a7470e10
S. Suzuki <terrierscript@gmail.com>
2022-07-03 22:50:04 +0900

    split layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,45 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { Box, ChakraProvider, Stack } from "@chakra-ui/react"
 import { AppProps } from "next/app"
-import React from "react"
+import { useRouter } from "next/router"
+import React, { FC, PropsWithChildren } from "react"
+
+const SubappLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
+  return <Stack>
+    <Box bg="blue.100">Sub app</Box>
+    <Box>
+      {children}
+    </Box>
+  </Stack>
+}
+
+const MainappLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
+  return <Stack>
+    <Box bg="red.100">Main app</Box>
+    <Box>
+      {children}
+    </Box>
+  </Stack>
+}
+
+const AppLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
+  const router = useRouter()
+  if (router.pathname.startsWith("/subapp")) {
+    return <SubappLayout>
+      {children}
+    </SubappLayout>
+  }
+
+  return <MainappLayout>
+    {children}
+  </MainappLayout>
+
+}
 
 function App({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
-    <Component {...pageProps} />
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
   </ChakraProvider>
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import { Box } from '@chakra-ui/react'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import React from 'react'
 
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  if (process.env.APP_MODE === "subapp") {
+    return {
+      redirect: {
+        destination: "/subapp",
+        statusCode: 301
+      }
+    }
+  }
+  return {
+    redirect: {
+      destination: "/mainapp",
+      statusCode: 301
+    }
+  }
+}
+
 export default function Home() {
-  return (
-    <Box>
-      <Head>
-        <title></title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </Box>
-  )
+  return null
 }
