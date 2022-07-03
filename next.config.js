2b1bad1d5498628fcb46869beeba4f95b2cc8788
S. Suzuki <terrierscript@gmail.com>
2022-07-03 18:34:20 +0900

    setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
-module.exports = {
-  experimental: {
-    esmExternals: false
+
+
+const subAppConfig = () => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  return {
+    distDir: ".next-subapp"
+  }
+}
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const baseAppConfig = {
+}
+
+module.exports = () => {
+  const appMode = process.env.APP_MODE
+  if (appMode === "SUBAPP") {
+    return {
+      ...baseAppConfig,
+      ...subAppConfig
+    }
+  }
+  /**
+   * @type {import('next').NextConfig}
+   */
+  return {
+    ...baseAppConfig
   }
 }
---
+++ src/pages/subapp/index.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react"
+
+const Page = () => {
+  return <Box>subapp!</Box>
+}
+export default Page
